b582059a4990a5d9e1b80bd9a65c40283611f883
crist2958 <145627417+crist2958@users.noreply.github.com>
2025-06-16 16:17:47 -0600

    Update token_validator.py

--- utils/token_validator.py
+++ utils/token_validator.py
@@ -1,3 +1,3 @@
 #secret key
-SECRET_KEY = '$2b$12$YAsCaru0O7FzbK7cH62hFuRnAWM6KYODzm1wekoliCk6jMNDm83vC'  # Reemplaza con tu clave real
+SECRET_KEY = ''  # Reemplaza con tu clave real
 #es hola hasheado con bycript XD
